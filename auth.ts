d039dd87926e3dbf2437eaf9559645729e65b301
nazar <nazar@botscrew.com>
2023-06-18 04:06:53 +0300

    removed todo

--- auth.ts
+++ auth.ts
@@ -17,11 +17,11 @@ export const {
         token.image = profile.picture
       }
       return token
+    },
+
+     authorized({ request, auth }) {
+     return !!auth?.user
     }
-    // @TODO
-    // authorized({ request, auth }) {
-    //   return !!auth?.user
-    // }
   },
   pages: {
     signIn: '/sign-in'
